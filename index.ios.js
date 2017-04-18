db417d1f4c131e87ecaba6c65bf1c2cb681c7a7f
Swapna Savant <swapnasavant@gmail.com>
2017-04-18 09:23:05 -0700

    change name

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class groove extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Groove!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
