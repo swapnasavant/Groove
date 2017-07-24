05ef55e6a412090f45aea60951d4b856a76eae39
Swapna Savant <swapnasavant@gmail.com>
2017-07-23 21:45:53 -0700

    TImeline-buyer

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -6,6 +6,8 @@ import {
   View,
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import { navigateTo } from '../actions/navigate';
 
 const propTypes = {
@@ -19,15 +21,49 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
     flexDirection: 'row',
   },
-  toolbarButton: {
-    width: 50,
+  toolbarHeader: {
+    backgroundColor: '#FF4331',
+    paddingTop: 30,
+    paddingBottom: 10,
+    flexDirection: 'row',
+    borderBottomColor: '#FFFFFF',
+    borderBottomWidth: 1,
+  },
+  toolbarAuto: {
+    backgroundColor: '#FF4331',
+    flexDirection: 'row',
+    paddingLeft: 50,
+    borderBottomColor: '#FFFFFF',
+    borderBottomWidth: 1,
+  },
+  inputButtonHome: {
+    paddingLeft: 10,
     color: '#fff',
     textAlign: 'center',
   },
-  toolbarTitle: {
+  inputYear: {
+    marginTop: -25,
     color: '#fff',
     textAlign: 'center',
+  },
+  dateButton: {
+    paddingTop: 15,
+    marginLeft: 10,
+    color: '#fff',
+  },
+  dayButton: {
+    fontSize: 30,
+  },
+  inputButtonNotify: {
+    paddingRight: 10,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  headerTitle: {
+    color: '#fff',
+    fontSize: 10,
     fontWeight: 'bold',
+    textAlign: 'center',
     flex: 1,
   },
   mainContainer: {
@@ -52,21 +88,43 @@ class Steps extends Component {
   }
 
   render() {
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
     return (
       <View style={styles.mainContainer}>
-        <View style={styles.toolbar}>
+        <View style={styles.toolbarHeader}>
           <TouchableOpacity
             onPress={() => this.handleButtonPress('hard')}
           >
-            <Text style={styles.toolbarButton} >
-                Home
+            <Text style={styles.inputButtonHome} >
+              <Icon name="md-home" size={15} />
             </Text>
           </TouchableOpacity>
-          <Text style={styles.toolbarTitle}>Home address</Text>
-          <Text style={styles.toolbarButton}>Notify</Text>
+          <Text style={styles.headerTitle}>
+            181 Whisman Dr.,
+            San Francisco, CA.
+          </Text>
+          <Text style={styles.inputButtonNotify} >
+            <Icon name="ios-notifications" size={15} />
+          </Text>
         </View>
-        <View style={styles.content}>
-          <Text>This is the content</Text>
+        <View style={styles.toolbar}>
+            <Text style={[styles.inputButtonHome, styles.dayButton]} >
+               {new Date().getDate()}
+            </Text>
+            <Text style={styles.inputButtonHome} >
+               {days[new Date().getDay()]}
+            </Text>
+            <Text style={styles.headerTitle}>
+              Hi Jack,
+              You have a task.
+            </Text>
+        </View>
+        <View style={styles.toolbarAuto}>
+          <Text style={styles.inputYear} >
+              {monthNames[new Date().getMonth()]} {new Date().getFullYear()}
+          </Text>
         </View>
       </View>
     );
